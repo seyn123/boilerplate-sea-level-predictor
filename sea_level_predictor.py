1db58b5b8229ed3db1d0851b253dd01ff0c49c68
seyn <80502655+seyn123@users.noreply.github.com>
2026-09-19 23:50:54 +0000

    answer for exercise

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -10,9 +10,7 @@ def draw_plot():
     # Create scatter plot
     fig, ax = plt.subplots()
     ax.scatter(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Sea Level (inches)')
-    ax.set_title('Rise in Sea Level')
+
     # Create first line of best fit
     lr = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
     intercept = lr.intercept
@@ -20,6 +18,7 @@ def draw_plot():
     intercept_range = np.linspace(df['Year'].min(), 2050, df.shape[0]+37)
     bestfit = slope * intercept_range + intercept
     ax.plot(intercept_range, bestfit, color="red", label="line of best fit 1")
+
     # Create second line of best fit
     lr = linregress(df.loc[(df['Year'] >= 2000)]['Year'], df.loc[(df['Year'] >= 2000)]['CSIRO Adjusted Sea Level'])
     intercept = lr.intercept
@@ -29,7 +28,9 @@ def draw_plot():
     ax.plot(intercept_range, bestfit, color="orange", label="line of best fit 2")
 
     # Add labels and title
-
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.set_title('Rise in Sea Level')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
